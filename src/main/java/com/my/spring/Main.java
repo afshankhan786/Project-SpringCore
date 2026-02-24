625cce073fc5ea84b9c6e580529ea8d5d858d0e4
afshankhan786 <profit.afshankhan@gmail.com>
2026-02-24 16:19:27 +0530

    update

--- src/main/java/com/my/spring/Main.java
+++ src/main/java/com/my/spring/Main.java
@@ -1,25 +1,26 @@
 package com.my.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.my.spring.model.Address;
 import com.my.spring.model.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		ApplicationContext ioc = new AnnotationConfigApplicationContext("com.my.spring");
 		
-		Employee bean = ioc.getBean("emp1",Employee.class);
-		System.out.println(bean);
+		Employee emp = ioc.getBean(Employee.class);
+		Address add = ioc.getBean(Address.class);
+
+		emp.setName("Anklus");
 		
-		Employee bean2 = ioc.getBean("emp2",Employee.class);
-		System.out.println(bean2);
-		
-		bean.setName("Bhanu");
-		System.err.println(bean);
- 
+	System.out.println(emp);
+	System.out.println(add);
+
 	}
 
 }
--- src/main/java/com/my/spring/model/Employee.java
+++ src/main/java/com/my/spring/model/Employee.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Employee {
 
 	private int id;
 
 	private String name, gender;
 
+	@Autowired
 	private Address address;
 
 
